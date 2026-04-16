6c16ffa61079f0d0d5f91e7e17374caf99afbba7
eldinbautista7 <ebauti14@my.centennialcollege.ca>
2026-04-15 20:13:13 -0400

    Remove old duplicate project folders

--- BACKEND-Booking-Platform/src/controllers/admin.controller.js
+++
@@ -1,7 +0,0 @@
-// src/controllers/admin.controller.js
-async function stats(req, res) {
-  // placeholder stats
-  res.json({ success: true, data: { users: 0, services: 0, bookings: 0 } });
-}
-
-module.exports = { stats };
